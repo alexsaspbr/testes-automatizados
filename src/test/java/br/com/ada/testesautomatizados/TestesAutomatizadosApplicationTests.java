2940fb831d89edade983b4748a8553c6cf23fe98
Alex Araujo <alexsaspbr@gmail.com>
2023-04-14 17:54:47 -0300

    initial commit

---
+++ src/test/java/br/com/ada/testesautomatizados/TestesAutomatizadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ada.testesautomatizados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesAutomatizadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
